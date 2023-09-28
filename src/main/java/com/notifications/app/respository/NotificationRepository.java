85f045b4784e4eb88fbecbab3334d3d72153031a
Santi Rodriguez <santimanuelr@gmail.com>
2023-09-28 18:18:21 -0300

    clean repositorys

--- src/main/java/com/notifications/app/respository/NotificationRepository.java
+++ src/main/java/com/notifications/app/respository/NotificationRepository.java
@@ -1,15 +1,11 @@
 package com.notifications.app.respository;
 
 import com.notifications.app.model.Notification;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findTop3ByUserIdOrderByCreationDateDesc(@Param("userId") String userId);
+    List<Notification> findTop3ByUserIdOrderByCreationDateDesc(String userId);
 
-    //List<SLSNotification> findByUserIdOrderBySNumber(@Param("userId") String userId, Pageable pageable);
 }
